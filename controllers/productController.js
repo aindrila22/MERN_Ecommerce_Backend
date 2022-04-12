1cf709c5c0dba04657275c1d09072ddfc1cd2d7d
aindrila22 <aindrilabhattacharjee72@gmail.com>
2022-04-12 20:50:07 +0530

    add search filter pagination

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,32 +1,41 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apiFeatures");
 
 //create product -- Admin
-exports.createProduct = async (req, res, next) => {
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
     product,
   });
-};
+});
 
 //get all products
-exports.getAllProducts = async (req, res) => {
-  const products = await Product.find();
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments();
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+
+  const products = await apiFeature.query;
   res.status(200).json({ success: true, products });
-};
+});
 
 //get single product details
-exports.getProductDetails = async (req, res, next) => {
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
   if (!product) {
     return next(new ErrorHandler("Product not found", 404));
   }
-  res.status(200).json({ success: true, product });
-};
+  res.status(200).json({ success: true, product, productCount });
+});
 
 //update product -- Admin
-exports.updateProduct = async (req, res, next) => {
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
   if (!product) {
     return next(new ErrorHandler("Product not found", 404));
@@ -37,10 +46,10 @@ exports.updateProduct = async (req, res, next) => {
     useFindAndModify: false,
   });
   res.status(200).json({ success: true, product });
-};
+});
 
 //delete product -- Admin
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
   if (!product) {
     return next(new ErrorHandler("Product not found", 404));
@@ -49,4 +58,4 @@ exports.deleteProduct = async (req, res, next) => {
   res
     .status(200)
     .json({ success: true, message: "Product deleted successfully" });
-};
+});
--- server.js
+++ server.js
@@ -2,12 +2,32 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
+//Handling uncaught exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("shutting down the server due to Uncaught Exception");
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
 //config
 dotenv.config({ path: "config/config.env" });
 
 //connecting to database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+
+//unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("shutting down the server due to Unhandled Promise Rejection");
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
